d14750b6b5bff6fdd483fdfdad65d55b4f4d9567
Andrey Cherepov <cherepov_aa@tomsk.lemz.ru>
2019-03-03 21:44:42 +0700

    add topic subscribe example

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,59 @@
 #include <iostream>
+#include <string>
 
 #include <signal.h>
+#include <unistd.h>
 #include <cstring>
 
 /* Mqtt library from contactless */
-#include <wbmqtt/version.h>
 #include <wbmqtt/mqtt_wrapper.h>
 
 #include <jsoncpp/json/json.h>
+
+/* Constant name of topic with trace information */
+const std::string TRACE_TOPIC = "/IEC104Server/ProtocolTrace";
+
+
+/* Global variables is wrong decision but temporary */
+PMQTTClient mqtt_client;
 
 /* Simple observer that print message with mqtt connection status */
 class TestObserver : public IMQTTObserver {
 public:    
     void OnConnect(int rc) override;
-    void OnMessage(const struct mosquitto_message *message) override {}
-    void OnSubscribe(int mid, int qos_count, const int *granted_qos) override {}
+    void OnMessage(const struct mosquitto_message *message) override;
+    void OnSubscribe(int mid, int qos_count, const int *granted_qos) override;
 };
 
 void TestObserver::OnConnect(int rc)
 {
-    std::cout << "Mqtt connect status: " << rc << std::endl;
+    mqtt_client->Publish( nullptr, TRACE_TOPIC, "OnConnect" );
+}
+
+void TestObserver::OnMessage(const struct mosquitto_message *message)
+{
+    mqtt_client->Publish( nullptr, TRACE_TOPIC, "OnMessage" );
+    
+    /* Create string and append information about incoming message topic */
+    std::string outMessage;
+    outMessage.append( "Message from: " );
+    outMessage.append( message->topic );
+
+    /* Append incoming message */
+    outMessage.append( "\nMessage: " );
+    outMessage.append( static_cast<const char*>(message->payload), message->payloadlen );
+    
+    /* Publish string with info about message to TRACE_TOPIC */
+    mqtt_client->Publish( nullptr, TRACE_TOPIC, outMessage );
+}
+
+void TestObserver::OnSubscribe(int mid, int qos_count, const int *granted_qos)
+{
+    mqtt_client->Publish( nullptr, TRACE_TOPIC, "OnSubscribe" );
 }
 
 /* Flag for signal processing */
-bool running = true;
+static bool running = true;
 
 /* Simple signal handler. Just set flag false */
 void sighndlr(int signal)
@@ -52,14 +82,12 @@ int main( int argc, char** argv )
 {
     set_sighandler();
 
-    std::cout << "Test MQTT" << std::endl;
-
-    /* create MQTT client */
+    /* create localhost MQTT client */
     TMQTTClient::TConfig mqtt_config;
     mqtt_config.Port = 1883;
     mqtt_config.Host = "localhost";
     mqtt_config.Id = "mqtt-iec104";
-    PMQTTClient mqtt_client = std::make_shared<TMQTTClient>(mqtt_config);
+    mqtt_client = std::make_shared<TMQTTClient>(mqtt_config);
 
     /* add observer to client */
     mqtt_client->Observe( std::make_shared<TestObserver>() );
@@ -67,10 +95,14 @@ int main( int argc, char** argv )
     mqtt_client->ConnectAsync();
     mqtt_client->StartLoop();
 
+    mqtt_client->Subscribe( nullptr, "/devices/hwmon/controls/CPU Temperature" , 0);
+
     while(running)
     {
-
-    };
+        /* Print test message every 5 seconds */
+        sleep(5);
+        mqtt_client->Publish( nullptr, TRACE_TOPIC, "Test wb MQTT" );
+    }
 
     return 0;
-};
+}
